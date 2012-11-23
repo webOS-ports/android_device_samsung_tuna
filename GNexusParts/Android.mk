3c9c3876af444ba84bcf115aa7241fd7193aae7b
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2012-11-23 01:58:37 -0200

    Disabling build for GNexusParts
    Change-Id: I9d2488c3fa60940e93994c3eb6840032fc9b735b
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

--- GNexusParts/Android.mk
+++ GNexusParts/Android.mk
@@ -10,6 +10,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := GNexusParts
 LOCAL_CERTIFICATE := platform
 
-include $(BUILD_PACKAGE)
+#include $(BUILD_PACKAGE)
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+#include $(call all-makefiles-under,$(LOCAL_PATH))
